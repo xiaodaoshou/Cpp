82ab5b0d563144b0572ba91eb5f3078e9371e14e
xiaodaoshou <1035011225@qq.com>
2021-12-03 18:20:30 +0800

    update

--- C++核心编程/指针/const与指针/const修饰常量.cpp
+++ C++核心编程/指针/const与指针/const修饰常量.cpp
@@ -7,6 +7,13 @@ void test() {
 	const int* p0 = &a;//常量指针
 	int * const p1 = &b;//指针常量
 	const int* const p2 = &c;//指向常量的指针常量
+	cout << "*p0的值为：" << *p0 << endl;;
+	a = 11;
+	cout << "*p0的值为：" << *p0 << endl;
+	*p1 = 21;
+	cout <<"*p2的值为：" << *p2 << endl;
+	c = 31;
+	cout << "*p2的值为：" << *p2 << endl;
 }
 int main() {
 	test();
